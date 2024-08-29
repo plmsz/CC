1a60faaa820c049a05d5e901715e9ea6721426f7
Paloma Souza <paloma.souza@ambevtech.com.br>
2024-08-29 13:05:39 -0300

    exemplo java

--- 0064-CAS/aula04_ex02.java
+++ 0064-CAS/aula04_ex02.java
@@ -2,20 +2,21 @@ import javax.swing.JOptionPane;
 
 public class aula04_ex02 {
     public static void soma() {
-        int[] nums = new int[5];
+        int[] numbers = new int[5];
+        int vetor[3];
         int soma = 0;
         for (int i = 0; i < 5; i++) {
-            nums[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite um número:"));
-            soma += nums[i];
+            numbers[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite um número:"));
+            soma += numbers[i];
        } 
-    JOptionPane.showMessageDialog(null, "Soma: " + nums[0] + " + " + nums[1] + " + " + nums[2] + " + " + nums[3] + " + " + nums[4] + " = " + soma);    
+    JOptionPane.showMessageDialog(null, "Soma: " + numbers[0] + " + " + numbers[1] + " + " + numbers[2] + " + " + numbers[3] + " + " + numbers[4] + " = " + soma);    
     }
     public static int produtoria() {
-        int[] nums = new int[5];
+        int[] numbers = new int[5];
         int potencia = 1;
         for (int i = 0; i < 5; i++) {
-            nums[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite um número:"));
-            potencia *= nums[i];
+            numbers[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite um número:"));
+            potencia *= numbers[i];
         }
         return potencia;
     }
